d37d5cc75f84b0dce1fdf352769e9f8c903e3c20
Melli1234d <58852401+Melli1234d@users.noreply.github.com>
2023-10-20 18:46:27 +0200

    Create Chatbot.jsx

---
+++ Chatbot.jsx
@@ -0,0 +1,100 @@
+function TestChatBot() {
+    const [customanswer, setCustomAnswer ] = useState('');
+    const [userQuestion, setUserQuestion ] = useState('');
+    const userID='react-chatbot-kit-user-chat-message'; //Class der Bot Nachricht (div)
+    const botID='react-chatbot-kit-chat-bot-message';// Class vom User (div)
+
+
+
+
+    function myDiv(divID, divValue){
+        const chatBox = document.querySelector('#chatbox');//chatbox id div setzen
+        const questionDiv= document.createElement('div'); //div element erstellen
+        questionDiv.className=divID; //id
+        questionDiv.innerHTML=divValue; //text
+        chatBox.appendChild(questionDiv); //neues div element erstellen
+        console.log("DIV VALUE:");
+        console.log(divValue);
+    }
+
+
+    function clearInputField() {
+        document.getElementById("messagefield").value = "";
+    }
+
+
+
+    const handleClick = () => {
+        console.log("handleClick:\n");
+        console.log(userQuestion);
+ //mögliche antworten auf die Fragen
+        answerQuestion(userQuestion, [
+            {id: 'doc1', text: 'Es gibt sehr viele Orte wo du Bücher hinspenden kannst. Welches Genre sind die Bücher?'},
+            {id: 'doc2', text: 'Darüber habe ich leider keine Kenntnisse. Kann ich dir sonst noch helfen?'},
+            // {id: 'doc3', text: 'Wenn du Bücher für Kinder kannst du da hin schicken: Kinderorganisationen:'},
+            {id: 'doc4', text: 'Alte Klamotten kannst du hier her spenden: '},
+            // {id: 'doc5', text: 'Du kannst alle möglichen Gegenstände spenden. Zum Beispiel kannst du: Bücher,DVDs,Klamotten spenden. Was möchtest du gerne spenden?'},
+            {id: 'doc6', text: 'Du kannst auch deine Bücher von einem abholservice abholen lassen.'},
+        ]).then(answers => {
+
+            // console.log(answers.answers[0].answer);
+            setCustomAnswer(answers.answers[0].answer); //setzen der Bot Nachricht
+            myDiv(getUserID(), userQuestion); //vom User die id für das div holen und in das Div den text vom input feld rein tun
+            myDiv(getBotID(), answers.answers[0].answer); //vom Bot die id für das div holen + texxt aus antwortmöglichkeiten
+        })
+        clearInputField();
+        // handleClick.push(userQuestion);
+        // handleClick.push(customanswer);
+
+    }
+    const handleChange = event => {
+        console.log("Am in handleChange and am using this funktion");
+        console.log(event.target.value);
+        setUserQuestion(event.target.value); //text von user nachricht setzen
+        document.getElementById('messageeingabe').value=event.target.value;
+    };
+
+    function getUserID(){
+        return userID; //die ID zurückgeben für das div user
+    }
+    function getBotID(){
+        return botID; //die ID zurückgeben für das div bot
+    }
+
+
+
+
+    return (
+
+        <div>
+            <div id="chatbox" >
+                <div className="kontaktname"> <p>Chatbot </p><div> <img  src={chatbotbild} alt="chatbotbild" height={60} width={60} /></div></div>
+                <div className={getBotID()}> Hallo, wie kann ich dir behilflich sein? </div>
+                {/*<div className="{getUserID()}"*/}
+                {/*     type="text"*/}
+                {/*     id="messageeingabe"*/}
+                {/*     name="message"*/}
+                {/*     onChange={handleChange}*/}
+                {/*     value={userQuestion}>{userQuestion}*/}
+                {/*</div>*/}
+                {/*<div className="{getBotID()}"*/}
+                {/*     type="text"*/}
+                {/*     id="messageeingabe"*/}
+                {/*     name="message"*/}
+                {/*     onChange={handleChange}*/}
+                {/*     value={customanswer}>{customanswer}*/}
+                {/*</div>*/}
+            </div>
+            <input className="inputfield"
+                type="text"
+                id="messagefield"
+                name="message"
+                onChange={handleChange}
+            />
+            <button onClick={handleClick}><AiOutlineSend className="message-send" size={20} color="#00158B"/></button>
+        </div>
+    );
+
+}
+
+export default TestChatBot;
